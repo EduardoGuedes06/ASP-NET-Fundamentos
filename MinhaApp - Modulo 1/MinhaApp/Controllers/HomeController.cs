9df4058d516d7349124305b5ebe6483fa4a5e705
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-25 14:42:15 -0300

    Entendendo-Os-Controllers

---
+++ MinhaApp - Modulo 1/MinhaApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaApp.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello World";
+        }
+    }
+}
