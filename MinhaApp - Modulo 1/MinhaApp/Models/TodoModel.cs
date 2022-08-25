812c465f4d2f19171886c899fa5680c877893046
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-25 14:24:19 -0300

    create-database

---
+++ MinhaApp - Modulo 1/MinhaApp/Models/TodoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MinhaApp.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
