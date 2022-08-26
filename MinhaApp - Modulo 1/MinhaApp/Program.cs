1878308908dcaac000c18e882415a59e8446f8f3
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-26 14:37:48 -0300

    MVC

--- MinhaApp - Modulo 1/MinhaApp/Program.cs
+++ MinhaApp - Modulo 1/MinhaApp/Program.cs
@@ -1,4 +1,10 @@
+using MinhaApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
