8d848ec409b18b6d96472a15d1d23c56caa12de9
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-30 14:19:52 -0300

    Versionamento

---
+++ MinhaApp - Modulo 2/MinhaApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaApp.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("health-check")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
